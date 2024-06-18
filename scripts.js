0a95eef1c6d1e7bb9c5bf0ab95b91bd358168667
DGzzzzz <douglaswinter75@gmail.com>
2024-06-17 21:57:31 -0300

    feat: implementado validação no input de pesquisa do NCM

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 document.querySelector('#btn').addEventListener('click', function() {
     let code = document.querySelector('#ncm').value;
-    axios.get(`https://brasilapi.com.br/api/ncm/v1?search=${code}`)
+
+    if(!/^\d{2,8}$/.test(code)) {
+        alert('O código do NCM deve conter de 2 a 8 dígitos');
+        return
+    }	
+
+    axios.get(`https://brasilapi.com.br/api/ncm/v1?${code}`)
     .then(function(response) {
         if (response.data.length > 0) {
             document.querySelector('#descricao').innerHTML = response.data[0].descricao;
             document.querySelector('#codigo').innerHTML = response.data[0].codigo;
             document.querySelector('#data-inicio').innerHTML = response.data[0].data_inicio;
             document.querySelector('#data-fim').innerHTML = response.data[0].data_fim;
+
+            document.querySelector('#ncm').value = '';
+
         } else {
             alert('Nenhum resultado encontrado');
         }
@@ -14,4 +23,8 @@ document.querySelector('#btn').addEventListener('click', function() {
     .catch(function(error) {
         console.log(error);
     });
+});
+
+document.querySelector('#ncm').addEventListener('input', function() {
+    this.value = this.value.replace(/[^0-9]/g, '');
 });
